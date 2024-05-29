be05b2fb33bc1656fd7b0963da6bee3a0396d431
titus <pomelo.huang.wt@gmail.com>
2024-05-29 14:09:47 +0800

    th54_flutter

---
+++ lib/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text(
+            "132",
+          ),
+        ),
+    );
+  }
+}
